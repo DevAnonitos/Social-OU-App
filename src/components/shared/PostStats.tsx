e3ab16b9dd2511d86d4a4a971c61831a949a2f9b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-23 14:24:44 +0700

    update:Add->Components-Shared

---
+++ src/components/shared/PostStats.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PostStats = () => {
+  return (
+    <div>PostStats</div>
+  );
+};
+
+export default PostStats;
---
+++ src/components/shared/index.ts
@@ -0,0 +1,7 @@
+export { default as Loader } from "./Loader";
+export { default as TopBar } from "./TopBar";
+export { default as BottomBar } from "./BottomBar";
+export { default as UserCard } from "./UserCard";
+export { default as LeftSideBar } from "./LeftSideBar";
+export { default as PostCard } from "./PostCard";
+export { default as PostStats } from "./PostStats";
