fe02d4a5354eb6c68048ad857b2da585538bcbbb
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-23 16:20:23 +0700

    update:Add->Pages-Link

---
+++ src/_root/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as HomePage } from "./HomePage";
+export { default as ExplorePage } from "./ExplorePage";
+export { default as ProfilePage } from "./ProfilePage";
+export { default as EditPost } from "./EditPost";
+export { default as CreatePost } from "./CreatePost";
