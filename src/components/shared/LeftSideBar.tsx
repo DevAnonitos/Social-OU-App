0599956a0dda32a157a33ae4c810d5eb84383c6e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-26 23:39:26 +0700

    update:Add->LeftSideBar

--- src/components/shared/LeftSideBar.tsx
+++ src/components/shared/LeftSideBar.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
+
+import { INavLink } from '@/types';
+import { Loader } from '.';
+
 
 const LeftSideBar = () => {
   return (
-    <div>LeftSideBar</div>
+    <>
+      <nav className='leftsidebar border-r-[1px] border-gray-700'>
+        <div className='flex flex-col gap-11'>
+          <Link to="/" className='flex gap-3 items-center'>
+            <img
+              src="/vite.svg"
+              alt="logo"
+              width={55}
+              height={36}
+            />
+            OUSA
+          </Link>
+        </div>
+      </nav>
+    </>
   );
 };
 
