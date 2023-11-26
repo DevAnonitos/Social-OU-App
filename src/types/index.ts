219662705b05d946da35bd6c96cdbdf2b067d352
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-26 22:02:36 +0700

    update:Add->Config-Type:

--- src/types/index.ts
+++ src/types/index.ts
@@ -19,5 +19,31 @@ export type INewPost = {
   file: File[];
   location?: string;
   tag?: string;
-}
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tag?: string;
+};
+
+export type IUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+
+export type INewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
 
