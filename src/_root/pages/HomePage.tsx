9ab68370292f423657853bc197f077693aaba5f3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-28 20:28:33 +0700

    update:Add->HomePage

--- src/_root/pages/HomePage.tsx
+++ src/_root/pages/HomePage.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Loader, PostCard, UserCard } from '@/components/shared';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <div className='flex flex-1'>
+        <div className='home-container'>
+          <div className='home-posts'>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
