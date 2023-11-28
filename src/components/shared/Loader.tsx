df2de8daf734bc0b22f9f9c9f5e95116c2018482
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-28 21:15:20 +0700

    update:Add->Loader-Components

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const Loader = () => {
     return (
-        <div>Loader</div>
+        <>
+            <div className='flex-center w-full'>
+                <img
+                    src="/assets/icons/loader.svg"
+                    alt="loader"
+                    width={24}
+                    height={24}
+                    className="animate-spin"
+                />
+            </div>
+        </>
     );
 };
 
