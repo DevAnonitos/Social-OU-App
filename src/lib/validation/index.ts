08ab9ff3100c9e5616321a4f81be6e8e8b6ee73a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-30 17:18:05 +0700

    update:Add->Validation-Obj

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,22 @@
+import * as z from "zod";
+
+// SignUpValidation
+export const SignUpValidation = z.object({
+
+});
+
+// SignInValidation
+export const SignInValidation = z.object({
+
+});
+
+// ProfileValidation
+export const ProfileValidation = z.object({
+
+});
+
+// PostValidation
+export const PostValidation = z.object({
+  
+});
+
