fda5843cb4817cf7f48843e9f55082476f6051d6
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-30 15:47:04 +0700

    update:Add->Create-AuthContext

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,10 @@
+import { useNavigate } from "react-router-dom";
+import { createContext, useContext, useEffect, useState } from "react";
+
+import { IUser } from "@/types";
+
+
+export  function AuthProvider ({ children }: { children: React.ReactNode }) {
+
+};
+
