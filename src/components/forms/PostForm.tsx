038dd355711d3e08b89f753d0610538d997c7b3d
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-11-30 16:56:32 +0700

    update:Add->Handlelogic-PostForm

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import * as z from "zod";
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+
+import { 
+  Form, 
+  FormControl, 
+  FormDescription, 
+  FormField, 
+  FormItem, 
+  FormLabel, 
+  FormMessage 
+} from '../ui';
+import { useToast } from '../ui';
+
+import { FileUploader, Loader } from '../shared';
+
 
 const PostForm = () => {
+
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  const handleSubmit = async () => {
+
+  };
+
   return (
     <div>PostForm</div>
   );
