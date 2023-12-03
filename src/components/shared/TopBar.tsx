5db5f80c5c63db831c4937cf60258e63c584d772
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-03 12:59:02 +0700

    update:Add->Button-TopBar

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -1,18 +1,38 @@
 import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
+import { Button } from '../ui';
 
 const TopBar = () => {
+
+  const navigate = useNavigate();
+
+
+
   return (
     <>
-      <nav className='topbar'>
+      <nav className='topbar border-b-[1px] border-gray-700'>
         <div className='flex-between py-4 px-5'>
           <Link to="/" className='flex gap-3 items-center'>
-
+            <img
+              src="/assets/images/logo.svg"
+              alt="logo"
+              width={130}
+              height={325}
+            />
           </Link>
 
           <div className='flex gap-4'>
-            
+            <Button
+              variant="ghost"
+              className="shad-button_ghost"
+              onClick={() => {}}
+            >
+                <img 
+                  src="/assets/icons/logout.svg" 
+                  alt="logout" 
+                />
+            </Button>
           </div>
         </div>
       </nav>
