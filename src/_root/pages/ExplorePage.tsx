1b2ff994b190b9110a78cca2fde0b23c5e09d287
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-05 23:25:27 +0700

    update:Add->ExplorePage

--- src/_root/pages/ExplorePage.tsx
+++ src/_root/pages/ExplorePage.tsx
@@ -6,7 +6,50 @@ import { Loader, GridPostList } from '@/components/shared';
 
 const ExplorePage = () => {
   return (
-    <div>ExplorePage</div>
+    <>
+      <div className='explore-container'>
+        <div className='explore-inner_container'>
+          <h2 className="h3-bold md:h2-bold w-full">
+            Search User Posts
+          </h2>
+          <div className='flex gap-1 px-4 w-full rounded-lg bg-dark-4'>
+            <img 
+              src="/assets/icons/search.svg" 
+              alt="search" 
+              width={24} 
+              height={24} 
+            />
+            <Input
+              type='text'
+              placeholder='Search posts'
+              className='explore-search'
+            />
+          </div>
+        </div>
+
+        <div className='flex-between w-full max-w-5xl mt-16 mb-7'>
+          <h3 className="body-bold md:h3-bold">
+            Popular Today
+          </h3>
+
+          <div className='flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer'>
+            <p className="small-medium md:base-medium text-light-2">
+              All
+            </p>
+            <img
+              src="/assets/icons/filter.svg"
+              width={20}
+              height={20}
+              alt="filter"
+            />
+          </div>
+        </div>
+
+        <div className='flex flex-wrap gap-9 w-full max-w-5xl'>
+
+        </div>
+      </div>
+    </>
   );
 };
 
