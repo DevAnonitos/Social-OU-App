cf9f534b409cd95e877333dd035cce3fbfa0af54
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-05 20:03:23 +0700

    update:Add->Content-HomePage

--- src/_root/pages/HomePage.tsx
+++ src/_root/pages/HomePage.tsx
@@ -7,8 +7,16 @@ const HomePage = () => {
       <div className='flex flex-1'>
         <div className='home-container'>
           <div className='home-posts'>
-
+            <h2 className='h3-bold md:h2-bold-left w-full'>
+              Home Feed
+            </h2>
           </div>
+        </div>
+
+        <div className='home-creators border-l-[1px] border-gray-700'>
+          <h3 className='h3-bold text-light-1'>
+            Top Creators✨
+          </h3>
         </div>
       </div>
     </>
