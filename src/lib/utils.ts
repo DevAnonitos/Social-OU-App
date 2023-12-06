cd907bbf1dcfedec4dca96479842950f91db5959
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-06 15:59:09 +0700

    update:Add->FormatDateString

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,25 @@ export function cn(...inputs: ClassValue[]) {
 };
 
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+export function formatDateString(dateString: string) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${formattedDate} at ${time}`;
+};
+
+export const checkIsLiked = (likeList: string[], userId: string) => {
+  return likeList.includes(userId);
+};
