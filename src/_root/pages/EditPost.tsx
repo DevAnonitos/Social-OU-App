c485aa449bf9b195b309add515618659e78dffd9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-06 13:18:39 +0700

    update:Add->ConfigEditPost

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader } from '@/components/shared';
+
+import PostForm from '@/components/forms/PostForm';
 
 const EditPost = () => {
+
+  const { id } = useParams();
+
+
   return (
-    <div>EditPost</div>
+    <>
+      <div className='flex flex-1'>
+        <div className='common-container'>
+          <div className='flex-start gap-3 justify-start w-full max-w-5xl'>
+            <img
+              src="/assets/icons/edit.svg"
+              width={36}
+              height={36}
+              alt="edit"
+              className="invert-white"
+            />
+            <h2 className="h3-bold md:h2-bold text-left w-full">
+              Edit Post
+            </h2>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
