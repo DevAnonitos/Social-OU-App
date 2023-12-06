9ba339847207338a9e30fee7329ee6db1bda93be
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-06 16:33:11 +0700

    update:Add->ConfigFileUploader

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,8 +1,47 @@
 import React, { useCallback, useState } from 'react';
+import { FileWithPath, useDropzone } from "react-dropzone";
 
-const FileUploader = () => {
+import { Button } from '../ui';
+import { convertFileToUrl } from '@/lib/utils';
+
+type FileUploaderProps = {
+  fieldChange: (files: File[]) => void;
+  mediaUrl: string;
+};
+
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+
+  const [file, setFile] = useState<File[]>([]);
+  const [fileUrl, setFileUrl] = useState<string>(mediaUrl);
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(convertFileToUrl(acceptedFiles[0]));
+    },
+    [file]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".png", ".jpeg", ".jpg"],
+    },
+  });
+
   return (
-    <div>FileUploader</div>
+    <>
+      <div 
+        {...getRootProps()}
+        className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'
+      >
+        <input 
+          {...getInputProps()} 
+          className="cursor-pointer" 
+        />
+      </div>
+    </>
   );
 };
 
