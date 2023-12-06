abc061468c301bdc971f5b11d478d689d29d9916
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-06 12:48:35 +0700

    update:Add->ContentSavedPage

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import { GridPostList, Loader } from '@/components/shared';
+
+
 const Saved = () => {
   return (
-    <div>Saved</div>
+    <>
+      <div className='saved-container'>
+        <div className='flex gap-2 w-full max-w-5xl'>
+          <img
+            src="/assets/icons/save.svg"
+            width={36}
+            height={36}
+            alt="edit"
+            className="invert-white"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">
+            Saved Posts
+          </h2>
+        </div>
+      </div>
+    </>
   );
 };
 
