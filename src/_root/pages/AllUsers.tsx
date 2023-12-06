ff414c1ed6d40b01e81c576cf1f84e0eb9e8e84b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-06 13:06:18 +0700

    update:Add->AllUserFetch

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useToast } from '@/components/ui';
+import { UserCard, Loader } from '@/components/shared';
 
 const AllUsers = () => {
+
+  const { toast } = useToast();
+  
   return (
-    <div>AllUsers</div>
+    <>
+      <div className='common-container'>
+        <div className='user-container'>
+          <h2 className="h3-bold md:h2-bold text-left w-full">
+            All Users
+          </h2>
+        </div>
+      </div>
+    </>
   );
 };
 
