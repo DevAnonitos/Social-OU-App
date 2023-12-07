92af80118b09c8d97ccdfa16dfdbe428c15f676e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-07 14:51:10 +0700

    update:Add->PostCard-ComponentsShare

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import { PostStats } from '.';
+
+type PostCardProps = {
+
+};
+
 const PostCard = () => {
   return (
-    <div>PostCard</div>
+    <>
+      <div className='post-card'>
+        <div className='flex-between'>
+          <div className='flex items-center gap-3'>
+            <Link to={`/profile/`}>
+              <img 
+                src="/assets/icons/profile-placeholder.svg" 
+                alt="Logo" 
+                className='w-12 lg:h-12 rounded-full' 
+              />
+            </Link>
+
+            <div className='flex flex-col'>
+              <p className='base-medium lg:body-bold text-light-1'>
+                {/* Name */}
+              </p>
+
+              <div className='flex-center gap-2 text-light-3'>
+                {/* Create at */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
