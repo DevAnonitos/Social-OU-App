e5712e2f47d0ad2374c23cd3d8202b14fc61df86
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-07 16:29:16 +0700

    update:Add->PostStats

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-const PostStats = () => {
+import { checkIsLiked } from '@/lib/utils';
+
+type PostStateProps = {
+  userId: string;
+};
+
+const PostStats = ({ userId }: PostStateProps) => {
+
+  const location = useLocation();
+  const [likes, setLikes] = useState<string>();
+  const [isSaved, setIsSaved] = useState(false);
+
+  const containerStyle = location.pathname.startsWith("/profile") ? "w-full" : "";
+
   return (
-    <div>PostStats</div>
+    <>
+      <div className={`flex justify-between items-center z-20 ${containerStyle}`}>
+        <div className='flex gap-2 mr-5'>
+          <img 
+            src={`/assets/icons/like.svg`} 
+            alt="Like" 
+            width={20} 
+            height={20} 
+            className='cursor-pointer' 
+          />
+          <p className='small-medium lg:base-medium'>
+            {/* Length like  */}
+          </p>
+        </div>
+
+        <div className='flex gap-2'>
+          <img 
+            src="/assets/icons/saved.svg" 
+            alt="share"
+            width={20}
+            height={20}
+            className='cursor-pointer'
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
