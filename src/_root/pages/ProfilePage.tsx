94815c7e0829b85e601eae2eb0b62fafe6def33f
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-14 16:26:28 +0700

    update:Add->ProfilePage

--- src/_root/pages/ProfilePage.tsx
+++ src/_root/pages/ProfilePage.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import { 
+  Route, 
+  Routes, 
+  Link, 
+  Outlet, 
+  useParams, 
+  useLocation, 
+} from 'react-router-dom';
+
+import { Button } from '@/components/ui';
+
+interface StabBlockProps {
+  value:  string | number;
+  label: string;
+}
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <>
+      <div className='profile_container'>
+        <div className='profile-inner_container'>
+          <img 
+            src="/assets/icons/profile-placeholder.svg" 
+            alt="creator"
+            className='w-28 h-28 lg:w-36 lg:h-36 rounded-full' 
+          />
+          <div className='flex flex-col flex-1 justify-between md:mt-2'>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
