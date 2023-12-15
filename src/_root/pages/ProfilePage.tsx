a611e94e35ad5045494e8c1fcb15f25f21d6ed04
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-15 23:40:16 +0700

    update:Add->ProfilePage

--- src/_root/pages/ProfilePage.tsx
+++ src/_root/pages/ProfilePage.tsx
@@ -26,7 +26,17 @@ const ProfilePage = () => {
             className='w-28 h-28 lg:w-36 lg:h-36 rounded-full' 
           />
           <div className='flex flex-col flex-1 justify-between md:mt-2'>
+            <div className='flex flex-col w-full'>
+              <h1 className='text-center xl:text-left h3-bold md:h1-semibold w-full'>
+                Nguyen Duc Gia Bao
+              </h1>
+              <p 
+                className='small-regular md:body-medium text-light-3 
+                text-center xl:text-left'
+              >
 
+              </p>
+            </div>
           </div>
         </div>
       </div>
