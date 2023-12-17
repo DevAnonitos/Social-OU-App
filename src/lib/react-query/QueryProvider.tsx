152d40f14d8de36458b0b3e3150ae443a0dcdd4c
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-17 19:29:19 +0700

    update:Add->QueryProvider

---
+++ src/lib/react-query/QueryProvider.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
+};
+
+export default QueryProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext.tsx';
+import QueryProvider from './lib/react-query/QueryProvider.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App /> 
+      <QueryProvider>
+        <App /> 
+      </QueryProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
