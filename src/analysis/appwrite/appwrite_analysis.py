f714e359f7a0cf5c13d53c3c9a10756ddd6dfc5b
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-20 13:49:19 +0700

    update:Add->AppwriteAnalys

--- src/analysis/appwrite/appwrite_analysis.py
+++ src/analysis/appwrite/appwrite_analysis.py
@@ -11,6 +11,9 @@ class AppwriteClient:
     self.project_id = project_id
     self.secret_key = secret_key
   
+  def __str__(self):
+    return f"AppwriteClient(endpoint={self.endpoint}, project_id={self.project_id}, secret_key={self.secret_key})"
+  
   # Ham lay data tu API
   def get_data(self, collection_id):
     session = requests.Session()
@@ -19,3 +22,12 @@ class AppwriteClient:
       'X-Appwrite-Project': self.project_id,
       'X-Appwrite-Key': self.secret_key,
     })
+    
+# Loading env file from env local app
+load_dotenv()
+
+appwrite_endpoint = os.getenv('APPWRITE_ENDPOINT')
+project_id = os.getenv('PROJECT_ID')
+secret_key = os.getenv('SECRET_KEY')
+
+client = AppwriteClient(appwrite_endpoint, project_id, secret_key)
