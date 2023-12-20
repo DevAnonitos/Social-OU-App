bab2b566a47226c40181c61b9d468748f37ea356
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-20 16:16:20 +0700

    update:Add->FileUploaderUI

--- src/analysis/appwrite/chart.py
+++ src/analysis/appwrite/chart.py
@@ -1,3 +1,5 @@
 import matplotlib.pyplot as plt
 import pandas
 import numpy as np
+
+import appwrite_analysis
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -40,6 +40,42 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
           {...getInputProps()} 
           className="cursor-pointer" 
         />
+
+        {fileUrl ? (
+          <>
+            <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
+              <img 
+                src={fileUrl} 
+                alt="Image"
+                className='file_uploader-img' 
+              />
+              <p className='file_uploader-label'>
+                Click or drag photo to replace
+              </p>
+            </div>
+          </>
+        ): (
+          <>
+            <div className='file_uploader-box'>
+              <img
+                src="/assets/icons/file-upload.svg"
+                width={96}
+                height={77}
+                alt="file upload"
+              />
+
+              <h3 className='base-medium text-light-2 mb-2 mt-6'>
+                Drag photo here
+              </h3>
+              <p className="text-light-4 small-regular mb-6">
+                SVG, PNG, JPG
+              </p>
+              <Button type="button" className="shad-button_dark_4">
+                Select from computer
+              </Button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
