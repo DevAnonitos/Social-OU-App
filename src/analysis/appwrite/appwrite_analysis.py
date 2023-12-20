506794089e6fad09c13694f2d13b16ff5cad6bd8
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-20 12:55:29 +0700

    update:Add->MutationQuery-TanstackQuery

--- src/analysis/appwrite/appwrite_analysis.py
+++ src/analysis/appwrite/appwrite_analysis.py
@@ -1,12 +1,21 @@
 import os
 import requests
 import json
+from dotenv import load_dotenv
 
-
-class AppwriteClient: 
+class AppwriteClient:
+  
+  # Ham khoi tao
   def __init__(self, appwrite_endpoint, project_id, secret_key):
     self.endpoint = appwrite_endpoint
     self.project_id = project_id
     self.secret_key = secret_key
-    
   
+  # Ham lay data tu API
+  def get_data(self, collection_id):
+    session = requests.Session()
+    session.headers.update({
+      'Content-Type': 'application/json',
+      'X-Appwrite-Project': self.project_id,
+      'X-Appwrite-Key': self.secret_key,
+    })
---
+++ src/lib/react-query/queries.ts
@@ -0,0 +1,31 @@
+import { 
+  useQuery, 
+  useMutation, 
+  useQueryClient, 
+  useInfiniteQuery 
+} from "@tanstack/react-query";
+
+import { QUERY_KEYS } from "./queryKeys";
+
+// Import Type
+import { 
+  INewPost, 
+  INewUser, 
+  IUpdatePost, 
+  IUpdateUser 
+} from "@/types";
+
+import { 
+  signInAccount,
+  signOutAccount,
+} from "../appwrite/api";
+
+
+export const useSignInAccount = () => {
+  return useMutation({
+    mutationFn: (user: { 
+      email: string, 
+      password: string 
+    }) => signInAccount(user),
+  });
+};
