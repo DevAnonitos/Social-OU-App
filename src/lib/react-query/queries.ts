55c0f0bec34899c2562ed9c3e8fcb210006acaf5
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-12-21 16:48:59 +0700

    update:Add->useSignOutAccount

--- src/lib/react-query/queries.ts
+++ src/lib/react-query/queries.ts
@@ -2,7 +2,7 @@ import {
   useQuery, 
   useMutation, 
   useQueryClient, 
-  useInfiniteQuery 
+  useInfiniteQuery, 
 } from "@tanstack/react-query";
 
 import { QUERY_KEYS } from "./queryKeys";
@@ -20,12 +20,19 @@ import {
   signOutAccount,
 } from "../appwrite/api";
 
-
+// SignInAccount
 export const useSignInAccount = () => {
   return useMutation({
     mutationFn: (user: { 
       email: string, 
       password: string 
     }) => signInAccount(user),
+  });
+};
+
+// SignOutAccount
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
   });
 };
