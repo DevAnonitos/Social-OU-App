55cf3fafb80bc13187d3f937711c8e11868195f7
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-06 15:47:02 +0700

    update:Add->Config-KeyAPI

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -11,6 +11,8 @@ export const appwriteConfig = {
   url: import.meta.env.VITE_APPWRITE_URL,
   databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
   userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
 };
 
 export const client = new Client();
