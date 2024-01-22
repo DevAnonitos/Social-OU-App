3cf5a3c777b09397bc0cb90014ae36d3a1446f88
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-01-22 23:56:39 +0700

    update:Add->StyleStatusPage

--- src/components/status/GlobalError.tsx
+++ src/components/status/GlobalError.tsx
@@ -2,11 +2,11 @@ import React, { Component, ErrorInfo, ReactNode } from 'react';
 
 interface GlobalErrorProps {
   children: ReactNode;
-}
+};
 
 interface GlobalErrorState {
   hasError: boolean;
-}
+};
 
 class GlobalError extends Component<GlobalErrorProps, GlobalErrorState> {
   constructor(props: GlobalErrorProps) {
@@ -27,10 +27,10 @@ class GlobalError extends Component<GlobalErrorProps, GlobalErrorState> {
     if (this.state.hasError) {
       // You can customize the error message or UI here
       return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen">
           <h1 className="text-4xl font-bold mb-4 text-red-500">Oops! Something went wrong.</h1>
           <p className="text-lg">
-            We apologize for the inconvenience. Please try again later.
+            We apologize for the inconvenience. Please try again later!
           </p>
         </div>
       );
@@ -38,6 +38,6 @@ class GlobalError extends Component<GlobalErrorProps, GlobalErrorState> {
 
     return this.props.children;
   }
-}
+};
 
 export default GlobalError;
